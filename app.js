52df93904a6ee936b4f40fd7e237728efbc54d91
EJ <tavorearljohn@gmail.com>
2023-12-29 20:24:17 +0800

    doing the task

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 
 //Static files
 app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/img', express.static(__dirname + 'public/img'))
 
 //Set Views
 app.set('views', './views')
--- routes/User.js
+++ routes/User.js
@@ -10,19 +10,21 @@ let logged_in_user_uuid = null;
 router.post('/sign-up', async (req, res) => {
   try {
     const { email, user_name, password } = req.body;
-    // Hash and salt the password
+    //Handle empty fields
+    if (!email || !user_name || !password) {
+      res.render('signup',{error:"Please fill in all fields"})
+    }
     const hashedPassword = await bcrypt.hash(password, 10);
     const user_uuid = uuid.v4();
-
     // Store the user in the database
     db.run('INSERT INTO users (user_uuid,email, user_name, hashed_password) VALUES (?,? ,?, ?)', [user_uuid,email, user_name, hashedPassword], function (err) {
       if (err) {
         console.error(err);
         return res.status(500).send('Internal Server Error');
       }
-      res.send(req.body);
+      //render login page after successful signup
+      res.render('login',{message:"User created successfully"})
     });
-
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
@@ -35,24 +37,41 @@ router.post('/sign-in', async (req, res) => {
   let user_name = req.body.user_name;
   const query = "SELECT * FROM users WHERE user_name = ?"
   db.get(query,[user_name],(err,row) =>{
-    if(row == undefined){
-      res.render('register')
+    //handle error so app wil not crash
+    if(err || !row){
+      console.error(err);
+      res.render('login',{error:"User not found"})
     }
     const {hashed_password,user_uuid} = row; 
     bcrypt.compare(user_entered_password, hashed_password, (err, result) => {
       if (err) {
-        // Handle error
         console.error(err);
       } else if (result) {
         logged_in_user_uuid = user_uuid;
-        res.render('homepage')
+        //use getPosts function to get all posts from database
+        getPosts().then((posts) =>{
+          res.render('homepage',{posts:posts})
+        })
         console.log('Authentication successful');
       } else {
-        // Passwords do not match, authentication failed
+        res.render('login',{error:"Password is incorrect or user not found"})
         console.log('Authentication failed');
       }
     }); 
   })
 });
+
+//function to get post from database
+function getPosts(){
+  return new Promise((resolve,reject) =>{
+    const query = "SELECT * FROM post_mock"
+    db.all(query,[],(err,rows) =>{
+      if(err){
+        reject(err)
+      }
+      resolve(rows)
+    })
+  })
+}
 
 module.exports = router
